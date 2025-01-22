7f8089518c8a39228b16954a45980538f031d5d0
jwoon0606 <jwoon0606@gmail.com>
2025-01-22 09:06:59 +0900

    User

--- src/main/java/com/example/practicespr/controller/rest/UserRestController.java
+++ src/main/java/com/example/practicespr/controller/rest/UserRestController.java
@@ -1,6 +1,46 @@
 package com.example.practicespr.controller.rest;
 
+import com.example.practicespr.domain.User;
+import com.example.practicespr.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/api/user")
+@RestController
 public class UserRestController {
 
+    private final UserService userService;
 
+    public UserRestController(UserService userService) {
+        this. userService = userService;
+    }
+
+    @PostMapping("")
+    public Long create(@RequestBody Map<String, Object> params) {
+        return userService.create(params);
+    }
+
+    @GetMapping("/list")
+    public List<User> List() {
+        return userService.list();
+    }
+
+    @GetMapping("")
+    public User detail(Long id) {
+        return userService.detail(id);
+    }
+
+    @PutMapping("")
+    public int update(@RequestBody Map<String, Object> params) {
+        userService.update(params);
+        return 200;
+    }
+
+    @DeleteMapping("")
+    public int delete(@RequestBody Long id) {
+        userService.delete(id);
+        return 200;
+    }
 }
--- src/main/java/com/example/practicespr/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/practicespr/service/impl/UserServiceImpl.java
@@ -11,30 +11,53 @@ import java.util.Map;
 @Service
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
-    public UserServiceImpl(UserRepository userRepository) {this.userRepository = userRepository;}
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public Long create(Map<String, Object> params) {
-        return null;
+        String username = (String) params.get("username");
+        User tempUser = userRepository.findByUsername(username);
+        if(tempUser != null) {
+            return (long) -200;
+        }
+
+        User user = new User();
+        user.setUsername((String) params.get("username"));
+        user.setPassword((String) params.get("password"));
+        user.setName((String) params.get("name"));
+        user.setNick((String) params.get("nick"));
+        user.setPhone((String) params.get("phone"));
+
+        userRepository.save(user);
+        return user.getId();
     }
 
     @Override
     public void update(Map<String, Object> params) {
-
+        Long id = Long.parseLong((String) params.get("id"));
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("no data"));
+        if(params.get("password") != null) { user.setPassword((String) params.get("password")); }
+        if(params.get("name") != null) { user.setName((String) params.get("name")); }
+        if(params.get("nick") != null) { user.setNick((String) params.get("nick")); }
+        if(params.get("phone") != null) { user.setPhone((String) params.get("phone")); }
+        userRepository.save(user);
     }
 
     @Override
     public void delete(Long id) {
-
+        User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("no data"));
+        userRepository.delete(user);
     }
 
     @Override
     public User detail(Long id) {
-        return null;
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("no data"));
     }
 
     @Override
     public List<User> list() {
-        return null;
+        return userRepository.findAll();
     }
 }
